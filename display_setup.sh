f0dd524884dfdc84ff9b47cd3fdd0aba7ff159fe
mv <michvett.mv@gmail.com>
2018-09-03 14:21:47 +0200

    display setup: add check for nouveau and adjust xrandr command for nouveau

--- display_setup.sh
+++ display_setup.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
 
-xrandr --setprovideroutputsource modesetting NVIDIA-0;
+check() {
+    lspci -k|grep "Kernel driver in use: nou.*";
+    echo $?;
+}
+
+if [[ $(check) == 0 ]];
+    then
+	# nouveau (see also https://nouveau.freedesktop.org/wiki/Optimus/)
+	xrandr --setprovideroffloadsink nouveau Intel;
+	# if second gpu output not accessible
+	# xrandr --setprovideroutputsource nouveau Intel
+    else
+	# nvidia
+	xrandr --setprovideroutputsource modesetting NVIDIA-0; # not necessarily needed
+fi
+
 xrandr --auto;
+
+unset check;
