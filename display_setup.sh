5bb4103711c48590082cab1589700a1877f7da8b
mv <michvett.mv@gmail.com>
2018-09-09 10:42:10 +0200

    xrandr: replace lspci check

--- display_setup.sh
+++ display_setup.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
 check() {
-    lspci -k|grep "Kernel driver in use: nou.*";
-    echo $?;
+python -c '
+import os
+
+SYSFS_PATH="/sys/bus/pci/devices"
+devs = os.listdir(SYSFS_PATH)
+gpus = [ "/sys/bus/pci/devices/"+i+"/" for i in devs if "0x03000" in open(SYSFS_PATH+"/"+i+"/class").read() ]
+
+_g = (True for i in gpus if "nouveau" in os.path.realpath(i+"driver"))
+
+for v in _g:
+  if True:
+    print(v)
+    exit(0)
+exit(1)
+'
 }
 
-if [[ $(check) == 0 ]];
+if [[ $(check) == True ]];
     then
 	# nouveau (see also https://nouveau.freedesktop.org/wiki/Optimus/)
 	xrandr --setprovideroffloadsink nouveau Intel;
